8ad6bae816bab431096ed1c9971a6fb00ddbb692
Mohammed Yassin <mohammed_yassin95@hotmail.com>
2022-09-10 15:02:13 +0200

    Refactor: create script + servers IaC fix

--- create.sh
+++ create.sh
@@ -19,6 +19,18 @@ Help()
 }
 
 ############################################################
+# Hold until network stack is created                                                     #
+############################################################
+STACK_STATUS="temp"
+Hold()
+{
+    while [ $STACK_STATUS != "CREATE_COMPLETE" ]; do
+        STACK_STATUS=$(aws cloudformation describe-stacks --stack-name $NETWORK_STACK_NAME --query Stacks[].StackStatus --output text)
+        sleep 10
+    done
+    echo "Create completed"
+}
+############################################################
 # Process the input options.                               #
 ############################################################
 if [ "$2" = "--update" ]; then
@@ -28,19 +40,23 @@ else
 fi
 if [ "$1" = "--network" ]; then
     if [ $NETWORK_STACK_NAME ]; then
-        aws cloudformation $operation --stack-name $NETWORK_STACK_NAME --template-body file://network.yml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION
+        aws cloudformation $operation --stack-name $NETWORK_STACK_NAME --template-body file://network.yml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION \
+        || aws cloudformation update-stack --stack-name $NETWORK_STACK_NAME --template-body file://network.yml  --parameters file://network-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION
+        Hold
     else
         >&2 echo "NETWORK_STACK_NAME environment variable is empty"; exit 1;
     fi
 elif [ "$1" = "--servers" ]; then
     if [ $SERVERS_STACK_NAME ]; then
-        aws cloudformation $operation --stack-name $SERVERS_STACK_NAME --template-body file://servers.yml  --parameters file://server-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION
+        aws cloudformation $operation --stack-name $SERVERS_STACK_NAME --template-body file://servers.yml  --parameters file://server-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION \
+        || aws cloudformation update-stack --stack-name $SERVERS_STACK_NAME --template-body file://servers.yml  --parameters file://server-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION
     else
         >&2 echo "SERVERS_STACK_NAME environment variable is empty"; exit 1;
     fi
 elif [ "$1" = "--database" ]; then
     if [ $SERVERS_STACK_NAME ]; then
-        aws cloudformation $operation --stack-name $DATABASE_STACK_NAME --template-body file://database.yml  --parameters file://database-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION
+        aws cloudformation $operation --stack-name $DATABASE_STACK_NAME --template-body file://database.yml  --parameters file://database-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION \
+        || aws cloudformation update-stack --stack-name $DATABASE_STACK_NAME --template-body file://database.yml  --parameters file://database-parameters.json --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$AWS_REGION        
     else
         >&2 echo "SERVERS_STACK_NAME environment variable is empty"; exit 1;
     fi
